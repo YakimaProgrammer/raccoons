75e315ecf80394b777733c06c6797701a52d38a1
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2021-12-16 20:08:09 -0800

    Added slide 3

--- src/Slides/3/index.js
+++ src/Slides/3/index.js
@@ -1,13 +1,18 @@
 import { SimpleSlide, BasicSlideshow } from "../../BasicSlideshow";
 
-import world from "../2/resources/world.svg";
-import raccoon from "../../resources/raccoon.svg";
+import woodlands from "./resources/ecology.fnal.gov/woodland.jpg";
+import wetlands from "./resources/ecology.fnal.gov/wetland.jpg";
+import tree from "./resources/slate.com/tree.webp";
 
 export function Slide3() {
   return (
-    <BasicSlideshow height="30vh">
-      <SimpleSlide src={raccoon} desc="RACCOON" />
-      <SimpleSlide src={world} desc="The world as we know it" />
-    </BasicSlideshow>
+    <div>
+      <h2>Habitat</h2>
+      <BasicSlideshow height="60vh">
+        <SimpleSlide src={woodlands} desc="Woodland areas are one of the prefered habitats for raccoons" alt="A deciduous forest with leaf litter and a small pool of water." />
+        <SimpleSlide src={wetlands} desc="Raccoons can also be found in wetland areas" alt="A panorama of a marshy forest. A thick, still layer of water covers everything, broken up by the numberous trees and bushes dotting the landscape." />
+        <SimpleSlide src={tree} desc="Raccoons are becoming increasingly prevalent in urban areas" alt="A raccoon stares at the camara while clinging to a tree. Buildings can be seen in the background." />
+      </BasicSlideshow>
+    </div>
   )
 }
