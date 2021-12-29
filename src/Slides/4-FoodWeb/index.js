5d5eba1413b5f0d49dd3a333ed6b8d5805f78079
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2021-12-29 15:05:39 -0800

    Made a really crappy HOC

--- src/Slides/4-FoodWeb/index.js
+++ src/Slides/4-FoodWeb/index.js
@@ -36,60 +36,67 @@ import style from "./index.module.css";
 const prey = [style.acorn, style.bud, style.crayfish, style.grass, style.mussel, style.apple, style.bird, style.cherry, style.corn, style.fish, style.frog, style.insect, style.mouse, style.reptile, style.trash];
 const predators = [style.cougar, style.coyote, style.dog, style.hawk, style.owl, style.snake, style.person];
 
-//LineTo is rather finicky, so I have to micromanage it along a little bit
-export class Slide4 extends Component {
+export class PageLoadWatcher extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {pagedLoaded: false};
+    this.state = {pageLoaded: false};
 
     if (document.readyState !== "complete") {
-      window.addEventListener("load", () => this.setState({pagedLoaded: true}));
+      window.addEventListener("load", () => this.setState({pageLoaded: true}));
     } else {
-      this.state.pagedLoaded = true;
+      this.setState({pageLoaded: true})
     }
 
-    window.addEventListener("resize", () => {this.setState({pagedLoaded: false}); this.setState({pagedLoaded: true})})
+    window.addEventListener("resize", () => {this.setState({pageLoaded: false}); this.setState({pageLoaded: true})});
   }
+
   render() {
-    return (
-      <div>
-        <h2>Raccoons in the food web</h2>
-        <div className={style.foodsContainer}>
-          <div className={style.foods}>
-            <img src={acorn} className={style.acorn} alt='' />
-            <img src={bud} className={style.bud} alt='' />
-            <img src={crayfish} className={style.crayfish} alt='' />
-            <img src={grass} className={style.grass} alt='' />
-            <img src={mussel} className={style.mussel} alt='' />
-            <img src={apple} className={style.apple} alt='' />
-            <img src={bird} className={style.bird} alt='' />
-            <img src={cherry} className={style.cherry} alt='' />
-            <img src={corn} className={style.corn} alt='' />
-            <img src={fish} className={style.fish} alt='' />
-            <img src={frog} className={style.frog} alt='' />
-            <img src={insect} className={style.insect} alt='' />
-            <img src={mouse} className={style.mouse} alt='' />
-            <img src={reptile} className={style.reptile} alt='' />
-            <img src={trash} className={style.trash} alt='' />
-          </div>
-        </div>
-
-        <img src={raccoon} className={style.bigRaccoon} alt="" />
-
-        <div className={style.foods}>
-          <img src={cougar} className={style.cougar} alt='' />
-          <img src={coyote} className={style.coyote} alt='' />
-          <img src={dog} className={style.dog} alt='' />
-          <img src={hawk} className={style.hawk} alt='' />
-          <img src={owl} className={style.owl} alt='' />
-          <img src={snake} className={style.snake} alt='' />
-          <img src={person} className={style.person} alt='' />
-        </div>
-
-        {this.state.pagedLoaded ? prey.map((p, index) => <LineTo key={index} from={p} to={style.bigRaccoon} delay={0} zIndex={-1} borderColor="black" />) : null}
-        {this.state.pagedLoaded ? predators.map((p, index) => <LineTo key={index} from={style.bigRaccoon} to={p} delay={0} zIndex={-1} borderColor="black" />) : null}
-      </div>
-    )
+    return <this.props.component pageLoaded={this.state.pageLoaded} {...this.props} />
   }
 }
+
+//LineTo is rather finicky, so I have to micromanage it along a little bit
+function Slide4Component(props) {
+  return (
+    <div>
+      <h2>Raccoons in the food web</h2>
+      <div className={style.foodsContainer}>
+        <div className={style.foods}>
+          <img src={acorn} className={style.acorn} alt='' />
+          <img src={bud} className={style.bud} alt='' />
+          <img src={crayfish} className={style.crayfish} alt='' />
+          <img src={grass} className={style.grass} alt='' />
+          <img src={mussel} className={style.mussel} alt='' />
+          <img src={apple} className={style.apple} alt='' />
+          <img src={bird} className={style.bird} alt='' />
+          <img src={cherry} className={style.cherry} alt='' />
+          <img src={corn} className={style.corn} alt='' />
+          <img src={fish} className={style.fish} alt='' />
+          <img src={frog} className={style.frog} alt='' />
+          <img src={insect} className={style.insect} alt='' />
+          <img src={mouse} className={style.mouse} alt='' />
+          <img src={reptile} className={style.reptile} alt='' />
+          <img src={trash} className={style.trash} alt='' />
+        </div>
+      </div>
+
+      <img src={raccoon} className={style.bigRaccoon} alt="" />
+
+      <div className={style.foods}>
+        <img src={cougar} className={style.cougar} alt='' />
+        <img src={coyote} className={style.coyote} alt='' />
+        <img src={dog} className={style.dog} alt='' />
+        <img src={hawk} className={style.hawk} alt='' />
+        <img src={owl} className={style.owl} alt='' />
+        <img src={snake} className={style.snake} alt='' />
+        <img src={person} className={style.person} alt='' />
+      </div>
+
+      {props.pageLoaded ? prey.map((p, index) => <LineTo key={index} from={p} to={style.bigRaccoon} delay={0} zIndex={-1} borderColor="black" />) : null}
+      {props.pageLoaded ? predators.map((p, index) => <LineTo key={index} from={style.bigRaccoon} to={p} delay={0} zIndex={-1} borderColor="black" />) : null}
+    </div>
+  )
+}
+
+export const Slide4 = () => <PageLoadWatcher component={Slide4Component} />;
